21feb81d74e219a78ef23c1e83e29df37083662c
mrstarch <64928752+mrstarch@users.noreply.github.com>
2021-09-27 12:08:10 -0700

    Lab2

--- main.py
+++ main.py
@@ -15,18 +15,59 @@
 # [START gae_python38_app]
 # [START gae_python3_app]
 from flask import Flask
-
+import os
+from google.cloud import datastore
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
 
+dataclient = datastore.Client()
+
 
 @app.route('/')
 def hello():
+    addVisitor()
     """Return a friendly HTTP greeting."""
     return 'Hello World! Daniel Strauch 300206805'
 
+@app.route('/version')
+def versA():
+    addVisitor()
+    return 'This is app version B!'
+
+@app.route('/instance')
+def getid():
+    addVisitor()
+    instanceid = os.getenv('GAE_INSTANCE')
+    return str(instanceid)
+
+@app.route('/version-id')
+def getversionid():
+    addVisitor()
+    versionid = os.getenv('GAE_VERSION')
+    return str(versionid)
+
+def addVisitor():
+    ent = dataclient.key('data', 'visitor')
+    total = dataclient.get(key=ent)
+    if total:
+        total['total'] += 1
+        dataclient.put(total)
+    else:
+        total = datastore.Entity(key=ent)
+        total['total'] = 0
+        dataclient.put(total)
+
+@app.route('/visitors')
+def getVisitor():
+    addVisitor()
+    ent = dataclient.key('data', 'visitor')
+    total = dataclient.get(key=ent)
+    if total:
+        return 'Total visitors: '+ str(total['total'])
+    else:
+        return 'Visitor Broken!'
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
